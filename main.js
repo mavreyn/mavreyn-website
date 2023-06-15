5795a64a76e164ed37d5e9dabe03e838451a0a27
Maverick Reynolds <mcreynolds02@gmail.com>
2023-06-15 15:09:46 -0400

    Explosive Hover

--- main.js
+++ main.js
@@ -10,8 +10,8 @@
  * 
 */
 
+// Typing animation for the landing page
 var doTypingAnimationAbout = true
-
 if (doTypingAnimationAbout) {
     var typing = new Typed(".about-words", {
         strings: ["Mathematics", "Astronomy", "Physics", "Comp Sci"],
@@ -22,17 +22,17 @@ if (doTypingAnimationAbout) {
     })
 }
 
+// Scroll reveal animations
 const sr = ScrollReveal({
     distance: "65px",
     duration: 2600,
     delay: 450,
     reset: true
 });
-
 sr.reveal(".landing-content", {delay: 200, origin: "top"});
 sr.reveal(".landing img", {delay: 600, origin: "right"});
-
 sr.reveal(".about-content", {delay: 50, origin: "left"});
+
 
 // Toggle the menu on mobile / small screens
 const navlist = $('.navbar');
@@ -42,3 +42,23 @@ menuIcon.click(() => {
     menuIcon.toggleClass('bx-x');
     navlist.toggleClass('open');
 })
+
+
+// Cool text animation
+//https://codepen.io/Hyperplexed/pen/mdjPzgM
+const enhance = id => {
+    const element = document.getElementById(id);
+    text = element.innerText.split("");
+
+    element.innerText = "";
+    text.forEach(letter => {
+        const span = document.createElement("span");
+        span.className = 'letter'
+        span.innerText = letter;
+        element.appendChild(span);
+    })
+}
+enhance("astronomy")
+enhance("computer-science")
+enhance("math")
+enhance("desmos")
